ff072125db5276805ab1fc00f5105829960d21e9
Fabricio C Zuardi <fabricio@fabricio.org>
2023-10-11 10:55:09 -0300

    test(jslib-aws):add test for creationDate swift bug
    See Swift Bugs:
    
    - https://bugs.launchpad.net/swift/+bug/1909213
    - https://bugs.launchpad.net/swift/+bug/1856938
    
    Related: STK-1455

--- src/k6/buckets.js
+++ src/k6/buckets.js
@@ -15,11 +15,18 @@ export function setup() {
 
 export default async function(data){
   // 3. VU code
+  const SWIFT_BUG_1856938_DATE = 1233679509000 //2009-02-03T16:45:09.000Z
   const buckets = await s3.listBuckets()
+  const testBuckets = buckets.filter(b => b.name === bucketName)
 
   check(buckets, {
     'list is array': (buckets) => Array.isArray(buckets),
     'list contains test bucket': (buckets) => buckets.map(b => b.name).includes(bucketName),
+  })
+
+  check(testBuckets, {
+    'test bucket have date different than Swift Bug #1856938': t => t[0].creationDate !== SWIFT_BUG_1856938_DATE,
+    'test bucket have date newer than 2009': t => new Date(t[0].creationDate).getUTCFullYear() > 2009 ,
   })
 }
 
