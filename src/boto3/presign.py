0446a29450b18707d047f5a8d223ffc492ab7168
Bruno Fernandes <bruno.fernandes1996@hotmail.com>
2023-10-24 16:16:28 -0300

    feat: add generate presign put url

--- src/boto3/presign.py
+++ src/boto3/presign.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python3
 
-print("Hello")
+import sys
+import boto3
+import yaml
+import boto3.session
+from botocore.config import Config
+
+profile_name = sys.argv[1]
+command_name = sys.argv[2]
+
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+endpoint_url = config['remotes'][profile_name]['s3']['endpoint']
+
+my_session = boto3.session.Session(profile_name=profile_name)
+my_config = Config(
+    signature_version = 'v4',
+)
+client = my_session.client(
+    service_name = 's3',
+    endpoint_url = endpoint_url,
+    config=my_config,
+)
+
+def generate_presigned_put_url():
+    s3_client = client
+    client_method = 'list_buckets'
+    try:
+        url = s3_client.generate_presigned_url(ClientMethod= client_method, HttpMethod="PUT",)
+    except:
+        print(
+            "Couldn't get a presigned URL for client method '%s'.", client_method
+        )
+        raise
+    return url
+
+if command_name == 'generate-put-url':
+    print(generate_presigned_put_url())
