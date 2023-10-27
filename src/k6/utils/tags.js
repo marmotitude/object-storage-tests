11b7c839dd47f6ae3fae0716239648b3d4d984f2
Fabricio Campos Zuardi <fabricio@fabricio.org>
2023-10-27 10:10:28 -0300

    add groups and tags to k6-jslib-aws-objects

--- src/k6/utils/tags.js
+++ src/k6/utils/tags.js
@@ -10,6 +10,8 @@ const tags = {
     // objects
     PUT_OBJECT: "put object",
     GET_OBJECT: "get object",
+    CREATE_MULTIPART: "create multipart",
+    ABORT_MULTIPART: "abort multipart",
 
     // temporary URLs
     CREATE_PRESIGN_PUT_URL: "create temporary upload URL",
@@ -31,6 +33,10 @@ const tags = {
     CLI_AWS_S3_MB: "aws s3 mb",
     CLI_AWS_S3_RB: "aws s3 rb",
     LIB_JS_K6_AWS_S3CLIENT_LIST_BUCKETS: "k6-jslib-aws S3Client.listBuckets",
+    LIB_JS_K6_AWS_S3CLIENT_PUT_OBJECT: "k6-jslib-aws S3Client.putObject",
+    LIB_JS_K6_AWS_S3CLIENT_GET_OBJECT: "k6-jslib-aws S3Client.getObject",
+    LIB_JS_K6_AWS_S3CLIENT_CREATE_MULTIPART: "k6-jslib-aws S3Client.createMultipartUpload",
+    LIB_JS_K6_AWS_S3CLIENT_ABORT_MULTIPART: "k6-jslib-aws S3Client.abortMultipartUpload",
   },
   fixes: {
     CREATION_DATE_2009_02_03: "OpenStack Swift Bug #1856938",
