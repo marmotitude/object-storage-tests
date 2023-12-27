9e981c8766be534a4e2a16d6e96d54f1f81345df
Fabricio Campos Zuardi <fabricio@fabricio.org>
2023-12-27 16:52:55 -0300

    feat: add more logging to track which commands are being executed

--- src/k6/utils/clis.js
+++ src/k6/utils/clis.js
@@ -4,6 +4,8 @@ const profileName = __ENV.AWS_CLI_PROFILE
 const profileName2 = `${__ENV.AWS_CLI_PROFILE}-second`
 
 export function aws({endpoint, region}, subCommand, args=[]){
+  console.log(`execute aws`)
+  console.log(`aws ${subCommand} --profile ${profileName} --endpoint ${endpoint} --region ${region} ${args.join(' ')}`)
   return exec.command("aws", [
     subCommand,
     "--profile", profileName,
@@ -25,6 +27,7 @@ export function aws2({endpoint, region}, subCommand, args=[]){
 
 export function swift({auth, user, key}={}, cmdName, args=[]){
   const authVersion = "1.0" // Specify a version for authentication.
+  console.log(`execute swift`)
   return exec.command("swift", [
     "--auth-version", authVersion,
     "--auth", auth,
@@ -36,6 +39,7 @@ export function swift({auth, user, key}={}, cmdName, args=[]){
 }
 
 export function mgc({region}, command, subCommand, args=[]){
+  console.log(`execute mgc`)
   return exec.command("mgc", [
     "object-storage",
     command,
@@ -45,6 +49,8 @@ export function mgc({region}, command, subCommand, args=[]){
   ])
 }
 export function rclone(subCommand, args=[]){
+  console.log(`execute rclone`)
+  console.log(`rclone ${subCommand} ${args.join(' ')}`)
   return exec.command("rclone", [
     subCommand,
     ...args,
